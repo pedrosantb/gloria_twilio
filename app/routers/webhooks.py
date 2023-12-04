75583073b76cd3bac67da12277aa91fcf82f4d1b
pedrosantb <pedrosantb@gmail.com>
2023-12-04 22:29:48 +0100

    Fix: Endpoint

--- app/routers/webhooks.py
+++ app/routers/webhooks.py
@@ -9,6 +9,24 @@ webhook_router = APIRouter(prefix="/webhook")
 
 @webhook_router.post("/gloriaListener")
 async def gloria_listener(request: Request):
+    # data_raw_json = await request.json()
+    # order_details = data_raw_json["orders"][0]
+
+    # client_phone = order_details['client_phone']
+    # client_first_name = order_details['client_first_name']
+    # order_type = order_details['type']
+    # order_status = order_details['status']
+
+    # message_body = f'Hello, {client_first_name}. Your order at Juice Guys is {order_status}'
+
+    # sms_client = Twilio()
+    # sms_status = await sms_client.send_message(client_phone, message_body)
+    # print(sms_status)
+    return status.HTTP_200_OK
+
+
+@webhook_router.get("/gloriaListener")
+async def gloria_listener_get(request: Request):
     data_raw_json = await request.json()
     order_details = data_raw_json["orders"][0]
 
@@ -18,7 +36,8 @@ async def gloria_listener(request: Request):
     order_status = order_details['status']
 
     message_body = f'Hello, {client_first_name}. Your order at Juice Guys is {order_status}'
-
+    print(order_status)
+    
     sms_client = Twilio()
     sms_status = await sms_client.send_message(client_phone, message_body)
     print(sms_status)
